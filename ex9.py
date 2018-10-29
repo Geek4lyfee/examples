32cc5032aa52f91c0d3001d9ec7bf648f7171f55
Geek4lyfee <amoli090@fiu.edu>
2018-10-29 09:56:24 -0400

    Update ex9.py
    finito

---
+++ ex9.py
@@ -0,0 +1,17 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days="Mon Tue Wed Thu Fri Sat Sun"
+months="Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days:",days)
+print("Here are the months:",months)
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+So if I wanted to keep going, I could write my life stories in Python
+Wow, I hope this ammuses someone. # Three of " = endless writing till im happy.. NOTE:Hashtags do not hide anything.
+""")
+#This is awesome
