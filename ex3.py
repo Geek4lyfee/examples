3998c1e65e664ff97973979950fc49abc3e0a268
Geek4lyfee <amoli090@fiu.edu>
2018-10-29 09:55:55 -0400

    Update ex3.py
    finished

---
+++ ex3.py
@@ -0,0 +1,24 @@
+print("I will now count my chickens:")
+# Prints normal
+print("hens",25 + 30 / 6)
+print("roosters", 100-25*3%4)
+# Calculates math 
+print("Now I will count the eggs:")
+# Prints
+print(3+2+1-5+4%2-1/4+6)
+# # calculates without showing anything but answer
+print("Is it true that 3 + 2 < 5 - 7?")
+# Displays ur calculations
+print(3+2<5-7)
+# diplays ur result, both needed for you to see it
+print("What is 3 + 2?",3+2)
+print("what is 5-7?",5-7)
+# calculates within same line, it condenses steps above into one line
+print("Oh, thats why its false, duh!.")
+# nonsense
+print("How about some more.")
+# more nonsense
+print("is it greater?",5>-2)
+print("is it greater or equal?",5>=-2)
+print("is it greater or equal?",5<=-2)
+# condensed calculations into one line
