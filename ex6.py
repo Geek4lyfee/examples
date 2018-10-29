ad08ae19bfe3ecb2f73b8baeb6f07c108190709c
Geek4lyfee <amoli090@fiu.edu>
2018-10-29 09:56:10 -0400

    Update ex6.py
    finale

---
+++ ex6.py
@@ -0,0 +1,24 @@
+types_of_people=10
+x=f"there are {types_of_people} types of people."
+#This is how u insert underscore formulations
+binary = "binary"
+do_not="don't"
+y=f"those who know {binary} and those who {do_not}."
+#mixing it up and setting variables differently, cool!
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+
+hilarious = False
+
+joke_evaluation="Isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious))
+# giving a variable a sentance, and a false statement a synonym=fals
+w="This is the left side of..."
+e="a string with a right side."
+# Adding string together, clever.
+print (w+e)
+#Before I submit this, i feel a sense of worry for a bad joke
