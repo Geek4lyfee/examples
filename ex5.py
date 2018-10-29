128146bba47f65cd43e830b3e21dcd2b0782d6bd
Geek4lyfee <amoli090@fiu.edu>
2018-10-29 09:56:03 -0400

    Update ex5.py
    done

---
+++ ex5.py
@@ -0,0 +1,20 @@
+my_name='Abner J. Molina'
+my_age = 29 # So a LIE
+my_height = 74 # Inches, another LIE
+my_weight=139 #lbs
+my_eyes='boring black' #BooHoo
+my_teeth='Bone Color' # Facts
+my_hair='messy, curly black'
+
+print(f"let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} LBS heavy.")
+print("actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"his teeth are usually {my_teeth} depending on the coffee.")
+
+#this line is tricky, try to get it exactly right... okay man 
+total=my_age+my_height+my_weight
+print(f"If i add {my_age},{my_height},and {my_weight} I get {total}.")
+
+#Blessed
