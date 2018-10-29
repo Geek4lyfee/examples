b1b4d02f2e7006c6389bf9bb68816b9560344bc2
Geek4lyfee <amoli090@fiu.edu>
2018-10-29 14:04:33 -0400

    Update exlib.py
    lib 1

---
+++ exlib.py
@@ -0,0 +1,11 @@
+#Mad Libs
+print("Type your name",end=' ')
+name=input()
+print("Type a part of the body",end=' ')
+body_part=input()
+print("Type your favorite drink",end=' ')
+favorite_drink=input()
+print("Type a substance",end=' ')
+substance=input()
+
+print(f"{name} is sick with the {body_part} flu. Drink more {favorite_drink} and take {substance} as needed.")
