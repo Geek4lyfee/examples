0d995fcb9acac031f99e36259c3ef7c145c477cb
Geek4lyfee <amoli090@fiu.edu>
2018-10-29 14:04:41 -0400

    Update exlib2.py
    lib 2

---
+++ exlib2.py
@@ -0,0 +1,41 @@
+#Mad Libs2
+print("What is your name?",end=' ')
+name=input()
+print("What is the most disgusting body fluid a human has?",end=' ')
+body_fluid=input()
+print("How tall are you?",end=' ')
+head=input()
+print("How much do you weigh?",end=' ')
+weight=input()
+print("What type of underwear do you wear?",end=' ')
+underwear=input()
+print("They build houses with b****, what’s the answer?",end=' ')
+bricks=input()
+print("Type a synonym for poop.",end=' ')
+poop1=input()
+print("Type another synonym for poop.",end=' ')
+poop2=input()
+print("Abeny is a little _______?",end=' ')
+fear=input()
+print("What is the world's most popular color?",end=' ')
+color=input()
+print("(Smell, Color, or Taste. Type one.",end=' ')
+SCT=input()
+print("How old are you?",end=' ')
+age=input()
+print("At what age do you wish to live to?",end=' ')
+death=input()
+
+print(f"""My name is {name}, I am retarded, and drink {body_fluid} for fun.
+my head is {head} wide, and weighs {weight} pounds. 
+My favorite hobby is smelling grandma {underwear}
+while shooting {bricks} with my cousins. 
+I love to eat {poop1}, and smell like {poop2} even more
+(you are what you eat!). 
+If I got a W today, I would scream like a little {fear}. 
+But since W’s are non-existent for me, 
+I know I won’t have to do that :).
+My favorite color is {color}, and that is the {SCT} of my diarrhea. 
+I am {age} years old, and want to live with my mom till I am {death} 
+This is my story, I hope you enjoy it.""")
+
